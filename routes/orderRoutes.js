3ff47172e25c7fe7ddb19fedc2f862199d5707b6
shahin1290 <s.patowary@yahoo.com>
2021-05-25 14:25:12 +0200

    refactors stripe checkout

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,31 +4,36 @@ const orderController = require('../controllers/orderController')
 const { AllowIfLogin, grantAccess } = require('../middleware/auth')
 
 router
-.route('/')
-.get(AllowIfLogin,grantAccess('readAny','order'),orderController.getAllOrders)
+  .route('/')
+  .get(
+    AllowIfLogin,
+    grantAccess('readAny', 'order'),
+    orderController.getAllOrders
+  )
 
+router
+  .route('/:bootcampId/klarna/order')
+  .post(AllowIfLogin, orderController.createKlarnaOrder)
+  .get(AllowIfLogin, orderController.ReadKlarnaOrder)
 
-  router
-    .route('/:bootcampId/klarna/order')
-    .post(AllowIfLogin,orderController.createKlarnaOrder)
-  .get(AllowIfLogin,orderController.ReadKlarnaOrder)
+router
+  .route('/push/:bootcampId/:userId')
+  .post(orderController.PushOrder)
+  .get(orderController.PushOrder)
 
-  router
-    .route('/push/:bootcampId/:userId')
-    .post(orderController.PushOrder)
-    .get(orderController.PushOrder)
-
-    router
-    .route('/capture/:bootcampId')
-    .get(AllowIfLogin,orderController.captureOrder)
-
+router
+  .route('/capture/:bootcampId')
+  .get(AllowIfLogin, orderController.captureOrder)
 
 router
   .route('/:bootcampId')
   .post(AllowIfLogin, orderController.createOrder)
-  .get(AllowIfLogin,orderController.ViewOrder)
-  router
-  .route('/myorders')
-  .get(AllowIfLogin, orderController.studentOrders)
+  .get(AllowIfLogin, orderController.ViewOrder)
+
+router
+  .route('/:bootcampId/stripe-payment-intent')
+  .post(AllowIfLogin, orderController.stripePaymentIntent)
+
+router.route('/myorders').get(AllowIfLogin, orderController.studentOrders)
 
 module.exports = router
